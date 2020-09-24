c1b42fe36315d0adf943862fc250f3d81ef5ebbe
AlexLGr <51243726+AlexLGr@users.noreply.github.com>
2020-09-24 19:13:10 +0600

    Last commit

--- ranker_dir/Search.java
+++ ranker_dir/Search.java
@@ -26,13 +26,17 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.json.JSONObject;
-import org.json.JSONException;
 
+// Main search engine class
 public class Search extends Configured implements Tool {
 	
+	// Class to create a vectorized representation of a query
 	public static class QueryVectorizor extends Mapper<LongWritable, Text, Text, Text>{
 		
+		// Storing the name of the query
 		public static String query = new String();
+		
+		// Storing word IDs that are encountered in a query
 		public static Set<Integer> query_indeces = new HashSet<Integer>();
 		
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -40,17 +44,21 @@ public class Search extends Configured implements Tool {
 				JSONObject obj = new JSONObject(s.substring(s.indexOf('{')));
 				String word = obj.getString("word");
 				
+				// Processing every word in a query and storing its ID
 				String [] query_split = query.split(" ");
 				for (String temp : query_split) {
+					temp.replaceAll("[^a-zA-Z0-9_-]", "");
 					if (temp.compareTo(word) == 0) {
-						query_indeces.add(Integer.parseInt(obj.getString("id")));
+						query_indeces.add(obj.getInt("id"));
 					}
 				} 
 		}		
 	}
 	
+	// Class to read lengths of files into memory to be used later
 	public static class LengthReader extends Mapper<LongWritable, Text, Text, Text>{
 		
+		// Map between word IDs and its length
 		public static Map<Integer, Integer> length_map = new HashMap<Integer, Integer>();
 		public static int total_length = 0;
 		public static int documents = 0;
@@ -66,12 +74,16 @@ public class Search extends Configured implements Tool {
 		}
 	}
 	
+	// Mapper for relevance function calculator
 	public static class RelevanceFunctionMap extends Mapper<LongWritable, Text, IntWritable, DoubleWritable> {
-
+		
+		// Preset parameters for Okapi BM25
 		public final static double b = 0.75;
 		public final static int k1 = 2;
 		
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+				
+				// Obtaining info about a current file and word
 				String s = value.toString();
 				JSONObject obj = new JSONObject(s.substring(s.indexOf('{')));
 				Integer doc_id = obj.getInt("wiki_id");
@@ -79,6 +91,7 @@ public class Search extends Configured implements Tool {
 				Double tf = obj.optDouble("tf");
 				Double tf_dtf = obj.optDouble("tf/idf");
 				
+				// Making sure that this word is present in a query and recording the relevance
 				if (QueryVectorizor.query_indeces.contains(word_id)) {
 					Integer d = LengthReader.length_map.get(doc_id);
 					Double avglen = ((double)LengthReader.total_length)/((double)LengthReader.documents);
@@ -90,6 +103,7 @@ public class Search extends Configured implements Tool {
 		}
 	}
 	
+	// Relevance function reducer that sums up all relevances for a particular document
 	public static class RelevanceFunctionReduce extends Reducer<IntWritable, DoubleWritable, IntWritable, DoubleWritable>{
 		public void reduce(IntWritable key, Iterable<DoubleWritable> list, Context context) throws java.io.IOException, InterruptedException {
 			Double sum = 0.0;
@@ -100,6 +114,7 @@ public class Search extends Configured implements Tool {
 		}
 	}
 	
+	// Mapper for ranking the documents according to the relevance
 	public static class MapRanker extends Mapper<LongWritable, Text, DoubleWritable, IntWritable>{
 		public void map(LongWritable key, Text value, Context context) throws java.io.IOException, InterruptedException {
 			String line = value.toString();
@@ -110,6 +125,8 @@ public class Search extends Configured implements Tool {
 		}
 	}
 	
+	// The ranking itself is performed in the intermediate step between map and reduce
+	// This class makes sure we output only the required number of document titles
 	public static class ReduceRanker extends Reducer<DoubleWritable, IntWritable, IntWritable, Text>{
 		
 		public static Integer top_N = 0;
@@ -128,11 +145,9 @@ public class Search extends Configured implements Tool {
 					        BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt2)));
 					        while(br.ready()==true) {
 								String line = br.readLine();
-								String [] split = line.split(" ");
-								Integer doc_id = Integer.parseInt(split[0]);
+								JSONObject obj = new JSONObject(line.substring(line.indexOf('{')));
+								Integer doc_id = obj.getInt("wiki_id");
 								if(doc_id == value.get()) {
-									String s = split[1];
-									JSONObject obj = new JSONObject(s.substring(s.indexOf('{')));
 									doc_title = obj.getString("title");
 									break;
 								}
@@ -148,6 +163,7 @@ public class Search extends Configured implements Tool {
 		}
 	}
 	
+	// Controller function for all the jobs
 	public int run(String [] args) throws Exception{
 		
 		JobControl jobControl = new JobControl("chain");
@@ -253,6 +269,7 @@ public class Search extends Configured implements Tool {
 	    return 0;
 	}
 	
+	// Main & handling of incorrect argument usage
 	public static void main(String[] args) throws Exception {
 		if (args.length!=3) {
 			System.out.printf("Incorrect number of arguments\nProper usage: "
